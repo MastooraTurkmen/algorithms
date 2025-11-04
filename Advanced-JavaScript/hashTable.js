63a475f504508afeb9ad00be919d80505e5cae40
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-11-04 16:32:28 +0430

    hash table

---
+++ Advanced-JavaScript/hashTable.js
@@ -0,0 +1,72 @@
+// Hash function
+let character = {
+    age: 20,
+    name: "Harry Potter",
+    muggle: false,
+    patronus: function () {
+        console.log("Expecto Patronum!");
+    }
+};
+
+character.age; // 20 // O(1)
+character.levitate = "Wingardium Leviosa!"; // O(1)
+character.patronus(); // Expecto Patronum! // O(1)
+
+
+
+
+// Create a Hash Table
+
+class HashTable {
+    constructor(size) {
+        this.data = new Array(size);
+        // this.data = [];
+    }
+
+    _hash(key) {
+        let hash = 0;
+        for (let i = 0; i < key.length; i++) {
+            hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+        }
+        return hash;
+    }
+
+    set(key, value) {
+        let address = this._hash(key);
+        if (!this.data[address]) {
+            this.data[address] = [];
+        }
+        this.data[address].push([key, value]);
+        return this.data;
+    }
+
+    get(key) {
+        const address = this._hash(key);
+        const currentBucket = this.data[address];
+        if (currentBucket) {
+            for (let i = 0; i < currentBucket.length; i++) {
+                if (currentBucket[i][0] === key) {
+                    return currentBucket[i][1];
+                }
+            }
+        }
+        return undefined;
+    }
+
+    keys() {
+        const keysArray = [];
+        for (let i = 0; i < this.data.length; i++) {
+            if (this.data[i]) {
+                keysArray.push(this.data[i][0][0]);
+            }
+        }
+        return keysArray;
+    }
+}
+
+const myHashTable = new HashTable(50);
+myHashTable.set("grapes", 10000);
+myHashTable.get("grapes");
+myHashTable.set("apples", 9);
+myHashTable.get("apples");
+myHashTable.keys();
