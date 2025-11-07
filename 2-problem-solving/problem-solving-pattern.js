85a819c4d5e1adb06bc86b8ffed64bd5d2c55b2a
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-11-07 21:43:12 +0430

    Sum Zero problem

--- 2-problem-solving/problem-solving-pattern.js
+++ 2-problem-solving/problem-solving-pattern.js
@@ -126,3 +126,27 @@ function validAnagram(str1, str2) {
 console.log(validAnagram('cinema', 'iceman')) // true
 console.log(validAnagram('hello', 'billion')) // false
 console.log(validAnagram('anagram', 'nagaram')) // true
+
+
+// Example: Sum Zero problem
+function sumZero(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left < right) {
+        let sum = arr[left] + arr[right];
+        if (sum === 0) {
+            return [arr[left], arr[right]];
+        } else if (sum > 0) {
+            right--;
+        } else {
+            left++;
+        }
+    }
+    return undefined;
+}
+
+// Example usage:
+console.log(sumZero([-3, -2, -1, 0, 1, 2, 3])) // [-3, 3]
+console.log(sumZero([-2, 0, 1, 3])) // undefined
+console.log(sumZero([1, 2, 3])) // undefined
